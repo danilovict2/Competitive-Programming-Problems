40dea7fbe69745a1966353770016349701739ff8
danilovict2 <danilosindric@gmail.com>
2022-06-19 19:45:53 +0200

    test

---
+++ test.cpp
@@ -0,0 +1,18 @@
+/*sortbysecond*/
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+using namespace std;
+
+ll n;
+string s;
+
+void solve(){
+    
+}
+
+int main(void){
+    
+}
