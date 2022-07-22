3005bda24410ec1a4d25c0977f4c8a4e963fd658
danilovict2 <danilosindric@gmail.com>
2022-07-22 17:37:47 +0200

    solved:

---
+++ ArpaAndAResearchInMexicanWave.cpp
@@ -0,0 +1,20 @@
+/*sortbysec*/
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+using namespace std;
+
+ll n,t,k;
+void solve(){
+	
+    
+}
+
+int main(void){
+    cin>>n>>k>>t;
+    int res = min(k,t);
+    int tmp = n+k-t;
+    res = min(res,tmp);
+    cout<<res<<endl;
+	return 0;
+}
