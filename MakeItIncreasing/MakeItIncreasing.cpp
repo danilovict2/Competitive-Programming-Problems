c46d432adb87672b7f5777c88bd187f15ef74a06
danilovict2 <danilosindric@gmail.com>
2022-12-23 18:54:50 +0100

    solved using brute force

---
+++ MakeItIncreasing/MakeItIncreasing.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define pii pair<int, int>
+#define vi vector<int>
+#define mp make_pair
+#define FOR(i, n) for (int i = 0; i < n; ++i)
+#define INF 0x3f3f3f3f
+template <typename... T>
+void read(T &...args)
+{
+	((cin >> args), ...);
+}
+template <typename... T>
+void write(T... args)
+{
+	((cout << args), ...);
+}
+const vector<pair<int, int>> nb = {{-1, -1}, {-1, 1}, {1, 1}, {1, -1}, {-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+const vector<pair<int, int>> nc = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+bool sortbysec(const pair<int, int> &a, const pair<int, int> &b) { return (a.second < b.second); }
+const int maxN = 30;
+
+
+int n, a[maxN];
+
+void solve(){
+	read(n);
+	FOR(i,n)read(a[i]);
+	int ans = 0;
+	for(int i=n-2;i>=0;--i){
+		while(a[i]>=a[i+1] && a[i]>0){
+			a[i]/=2;
+			++ans;
+		}
+		if(a[i] == a[i+1]){
+			write("-1\n");
+			return;
+		}
+	}
+	write(ans,"\n");
+}
+
+int main(void){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+	int t;
+	read(t);
+	while(t--)solve();
+	return 0;
+}
