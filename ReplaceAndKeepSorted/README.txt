8562447f7020d331bc232755d6358e8cd09f04cb
danilovict2 <danilosindric@gmail.com>
2022-12-30 19:33:48 +0100

    solved math problem

---
+++ ReplaceAndKeepSorted/README.txt
@@ -0,0 +1,15 @@
+Given a positive integer k, two arrays are called k
+
+-similar if:
+
+    they are strictly increasing;
+    they have the same length;
+    all their elements are positive integers between 1
+
+and k
+
+    (inclusive);
+    they differ in exactly one position. 
+
+You are given an integer k
+, a strictly increasing array a and q queries. For each query, you are given two integers li≤ri. Your task is to find how many arrays b exist, such that b is k-similar to array [ali,ali+1…,ari]. 
---
+++ ReplaceAndKeepSorted/ReplaceAndKeepSorted.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define pii pair<int, int>
+#define vi vector<int>
+#define mp make_pair
+#define forn(i, j, n) for (int i = (j); i < n; ++i)
+#define INF 0x3f3f3f3f
+template <typename... T>
+void read(T &...args)
+{
+	((cin >> args), ...);
+}
+template <typename... T>
+void write(T... args)
+{
+	((cout << args), ...);
+}
+const vector<pair<int, int>> nb = {{-1, -1}, {-1, 1}, {1, 1}, {1, -1}, {-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+const vector<pair<int, int>> nc = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+bool sortbysec(const pair<int, int> &a, const pair<int, int> &b) { return (a.second < b.second); }
+const int maxN = 1e5;
+
+int n,q,k;
+
+void solve(){
+	vi a(n);
+	for(auto &i:a)read(i);
+	int l,r;
+	while(q--){
+		read(l,r);
+		write(k+(a[r-1]-a[l-1] + 1) - 2*(r-l+1),"\n");
+	}
+}
+
+int main(void){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+	read(n,q,k);
+	solve();
+	return 0;
+}
