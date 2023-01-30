a212db47837a4ccde7e89f6245322327f2480d34
danilovict2 <danilosindric@gmail.com>
2023-01-30 18:39:13 +0100

    solved adhoc

---
+++ PetyaAndStaircases/PetyaAndStaircases.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define pii pair<int, int>
+#define vi vector<int>
+#define vii vector<pair<int,int>>
+#define mp make_pair
+#define sz(a) a.size() 
+#define forn(i, n) for (int i = 0; i < n; ++i)
+#define INF 0x3f3f3f3f
+template <typename... T>
+void read(T &...args)
+{
+	((cin >> args), ...);
+}
+template <typename... T>
+void write(T... args)
+{
+	((cout << args), ...);
+}
+const vector<pair<int, int>> nb = {{-1, -1}, {-1, 1}, {1, 1}, {1, -1}, {-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+const vector<pair<int, int>> nc = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+bool sortbysec(const pair<int, int> &a, const pair<int, int> &b) { return (a.second < b.second); }
+const int maxN = 1e5+1;
+
+int n,m;
+
+void solve(){
+	int a[m];
+	forn(i,m)read(a[i]);
+	sort(a,a+m);
+	forn(i,m-2){
+		if(a[i]+1 == a[i+1] && a[i]+2 == a[i+2]){write("NO\n");return;}
+	}
+	if(a[m-1] == n || a[0] == 1)write("NO\n");
+	else write("YES\n");
+}
+
+
+int main(void){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+	read(n,m);
+	solve();
+	return 0;
+}
