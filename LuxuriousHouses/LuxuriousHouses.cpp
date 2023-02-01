fb860b45c6c5f2b4861f2d7cde606c466a406fdc
danilovict2 <danilosindric@gmail.com>
2023-02-01 19:55:40 +0100

    solved adhoc

---
+++ LuxuriousHouses/LuxuriousHouses.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define pii pair<int, int>
+#define vi vector<int>
+#define vii vector<pair<int,int>>
+#define mp make_pair
+#define sz(a) a.size() 
+#define forn(i, n) for (int i = 0; i < n; ++i)
+#define INF 0x3f3f3f3f
+template <typename... T>
+void read(T &...args)
+{
+	((cin >> args), ...);
+}
+template <typename... T>
+void write(T... args)
+{
+	((cout << args), ...);
+}
+const vector<pair<int, int>> nb = {{-1, -1}, {-1, 1}, {1, 1}, {1, -1}, {-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+const vector<pair<int, int>> nc = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+bool sortbysec(const pair<int, int> &a, const pair<int, int> &b) { return (a.second < b.second); }
+const int maxN = 1e5+1;
+
+int n;
+
+void solve(){
+	int h[n];
+	forn(i,n)read(h[i]);
+	int mx = h[n-1];
+	vi ans;
+	ans.pb(0);
+	for(int i=n-2;i>=0;--i){
+		if(h[i] > mx){
+			ans.pb(0);
+			mx = h[i];
+		}else ans.pb(abs(h[i]-mx)+1);
+	}
+	reverse(ans.begin(),ans.end());
+	for(int i=0;i<ans.size();++i)write(ans[i]," ");
+	write("\n");
+}
+
+
+int main(void){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+	read(n);
+	solve();
+	return 0;
+}
