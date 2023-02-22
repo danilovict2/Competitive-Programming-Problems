769aac1b997fd484225099a6507f2f8a279d8eff
danilovict2 <danilosindric@gmail.com>
2023-02-22 21:02:37 +0100

    solved using greedy approach

---
+++ GameWithTelephoneNumbers/GameWithTelephoneNumbers.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define pii pair<int, int>
+#define vi vector<int>
+#define vii vector<pair<int,int>>
+#define mp make_pair
+#define sz(a) a.size() 
+#define forn(i, n) for (int i = 0; i < n; ++i)
+#define INF 0x3f3f3f3f
+template <typename... T>
+void read(T &...args)
+{
+	((cin >> args), ...);
+}
+template <typename... T>
+void write(T... args)
+{
+	((cout << args), ...);
+}
+const vector<pair<int, int>> nb = {{-1, -1}, {-1, 1}, {1, 1}, {1, -1}, {-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+const vector<pair<int, int>> nc = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+bool sortbysec(const pair<int, int> &a, const pair<int, int> &b) { return (a.second < b.second); }
+const int maxN = 5001;
+
+
+
+void solve(){
+	int n;
+	string s;
+	read(n,s);
+	int cnt1 = (n-11)/2;
+	int cnt2 = cnt1;
+	string res;
+	for(int i=0;i<n;++i){
+		if(s[i] == '8'){
+			if(cnt1 > 0)--cnt1;
+			else res+=s[i];
+		}else{
+			if(cnt2 > 0)--cnt2;
+			else res+=s[i];
+		}
+	}
+	if(res[0] == '8')write("YES\n");
+	else write("NO\n");
+}
+
+
+int main(void){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+	solve();
+	return 0;
+}
+
