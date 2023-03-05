264c35d357c2bbc9a22ddfd7864e12d79b135394
danilovict2 <danilosindric@gmail.com>
2023-03-05 15:32:43 +0100

    solved adhoc

---
+++ Lecture/Lecture.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define pii pair<int, int>
+#define vi vector<int>
+#define vii vector<pair<int,int>>
+#define mp make_pair
+#define sz(a) a.size() 
+#define forn(i, n) for (int i = 0; i < n; ++i)
+#define INF 1e18
+template <typename... T>
+void read(T &...args)
+{
+	((cin >> args), ...);
+}
+template <typename... T>
+void write(T... args)
+{
+	((cout << args), ...);
+}
+const vector<pair<int, int>> nb = {{-1, -1}, {-1, 1}, {1, 1}, {1, -1}, {-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+const vector<pair<int, int>> nc = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+bool sortbysec(const pair<int, int> &a, const pair<int, int> &b) { return (a.second < b.second); }
+const int maxN = 2*1e5+1;
+
+
+
+void solve(){
+	int n,m;
+	read(n,m);
+	map<string,pair<string,string>> mp;
+	for(int i=0;i<m;++i){
+		string s,t;
+		read(s,t);
+		mp[s] = {s,t};
+	}
+	for(int i=0;i<n;++i){
+		string s;
+		read(s);
+		if(mp[s].first.size() <= mp[s].second.size())write(s," ");
+		else write(mp[s].second," ");
+	}	
+}
+
+
+int main(void){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+	solve();
+	return 0;
+}
+
