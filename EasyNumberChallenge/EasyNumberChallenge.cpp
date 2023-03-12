39b3042283087c9a3088f2084ef424b13241fc88
danilovict2 <danilosindric@gmail.com>
2023-03-12 18:23:54 +0100

    solved adhoc

---
+++ EasyNumberChallenge/EasyNumberChallenge.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define pii pair<int, int>
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define mp make_pair
+#define sz(a) a.size()
+#define forn(i, n) for (int i = 0; i < n; ++i)
+#define INF 1e18
+template <typename... T>
+void read(T &...args){
+	((cin >> args), ...);
+}
+template <typename... T>
+void write(T... args){
+	((cout << args), ...);
+}
+const vector<pair<int, int>> nb = {{-1, -1}, {-1, 1}, {1, 1}, {1, -1}, {-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+const vector<pair<int, int>> nc = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+bool sortbysec(const pair<int, int> &a, const pair<int, int> &b) { return (a.second < b.second); }
+const int maxN = 2 * 1e5 + 1;
+
+
+
+void solve(){
+	int a,b,c;
+	read(a,b,c);
+	int d[a*b*c+1]={0};
+	for(int i=1;i<=a*b*c;++i){
+		for(int j=i;j<=a*b*c;j+=i)
+			++d[j];
+	}
+	ll ans = 0,mod=1073741824;
+	for(int i=1;i<=a;++i){
+		for(int j=1;j<=b;++j){
+			for(int k=1;k<=c;++k){
+				ans+=d[i*j*k];
+				ans %= mod;
+			}
+		}
+	}
+	write(ans,"\n");
+}
+
+int main(void){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+	solve();
+	return 0;
+}
---
+++ EasyNumberChallenge/README.txt
@@ -0,0 +1,3 @@
+Let's denote d(n) as the number of divisors of a positive integer n. You are given three integers a, b and c. Your task is to calculate the following sum:
+
+Find the sum modulo 1073741824 (230).
