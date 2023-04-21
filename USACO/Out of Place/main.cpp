31bc9288f3094db19677e7675ab04ae91d24de07
danilovict2 <danilosindric@gmail.com>
2023-04-21 21:02:32 +0200

    solved using greedy approach

---
+++ USACO/Out of Place/main.cpp
@@ -0,0 +1,59 @@
+/*
+ID: danilos4
+LANG: C++
+TASK: outofplace
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define pii pair<int, int>
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define MOD 9901
+#define mp make_pair
+#define sz(a) a.size()
+#define forn(i, n) for (int i = 0; i < n; ++i)
+#define INF 1e18
+ofstream fout ("outofplace.out");
+ifstream fin ("outofplace.in");
+template <typename... T>
+void read(T &...args){
+	((fin >> args), ...);
+}
+template <typename... T>
+void write(T... args){
+	((fout << args), ...);
+}
+const vector<pair<int, int>> dirs1 = {{-1, -1}, {-1, 1}, {1, 1}, {1, -1}, {-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+const vector<pair<int, int>> dirs2 = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+bool sortbysec(const pair<int, int> &a, const pair<int, int> &b) { return (a.second < b.second); }
+const int maxN = 32;
+
+int n;
+
+void solve(){
+	read(n);
+	int a[n], sorted[n];
+	forn(i,n){
+		read(a[i]);
+		sorted[i] = a[i];
+	}
+	sort(sorted,sorted+n);
+	int ans = 0;
+	forn(i,n){
+		ans += sorted[i] != a[i];
+	}
+	write(ans-1,'\n');
+}
+
+
+int main(void){
+	ios::sync_with_stdio(0);
+	fin.tie(0);
+	fout.tie(0);
+	solve();
+	return 0;
+}
