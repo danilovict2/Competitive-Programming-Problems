ad72962190220b756c71d4c901c89771a5e957be
danilovict2 <danilosindric@gmail.com>
2023-04-26 21:42:33 +0200

    solved using greedy approach

---
+++ CSES/Stick Divisions/main.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define pii pair<int, int>
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define mp make_pair
+#define sz(a) a.size()
+#define MOD 1000000007
+#define forn(i, n) for (int i = 0; i < n; ++i)
+#define INF 1e18
+template <typename... T>
+void read(T &...args){
+	((cin >> args), ...);
+}
+template <typename... T>
+void write(T... args){
+	((cout << args), ...);
+}
+const vector<pair<int, int>> dirs1 = {{-1, -1}, {-1, 1}, {1, 1}, {1, -1}, {-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+const vector<pair<int, int>> dirs2 = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+bool sortbysec(const pair<int, int> &a, const pair<int, int> &b) { return (a.second < b.second); }
+const int maxN = 2 * 1e5 + 10;
+
+
+void solve(){
+	int n,x;
+	read(x,n);
+	int a[n];
+	priority_queue<int> pq;
+	forn(i,n){
+		read(a[i]);
+		pq.push(-a[i]);
+	}
+	ll ans = 0;
+	for(int i=1;i<n;++i){
+		int a = -pq.top();
+		pq.pop();
+		int b = -pq.top();
+		pq.pop();
+		pq.push(-(a+b));
+		ans += a+b;
+	}
+	write(ans,'\n');
+}
+
+int main(void){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+	solve();
+	return 0;
+}
