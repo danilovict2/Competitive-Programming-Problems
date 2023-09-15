7546172aa2f47b7691d5e07ec1d197290b717cad
danilovict2 <danilosindric@gmail.com>
2023-09-15 19:55:57 +0200

    solved using complete search

---
+++ USACO/Milk Pails/main.cpp
@@ -0,0 +1,56 @@
+/*
+ID: danilos4
+LANG: C++
+TASK: pails
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define pii pair<int, int>
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define MOD 9901
+#define mp make_pair
+#define sz(a) a.size()
+#define forn(i, n) for (int i = 0; i < n; ++i)
+#define INF 1e18
+ofstream fout("pails.out");
+ifstream fin("pails.in");
+template <typename... T>
+void read(T &...args)
+{
+	((fin >> args), ...);
+}
+template <typename... T>
+void write(T... args)
+{
+	((fout << args), ...);
+}
+const vector<pair<int, int>> dirs1 = {{-1, -1}, {-1, 1}, {1, 1}, {1, -1}, {-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+const vector<pair<int, int>> dirs2 = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+bool sortbysec(const pair<int, int> &a, const pair<int, int> &b) { return (a.second < b.second); }
+const int maxN = 101;
+
+
+void solve(){
+	int x,y,m;
+	read(x,y,m);
+	int ans = 0;
+	for(int xPour=0;xPour*x<=m;++xPour) {
+		for(int yPour=0;yPour*y + xPour*x <= m;++yPour) {
+			ans = max(ans, xPour*x + yPour*y);
+		}
+	}
+	write(ans,'\n');
+}
+
+int main(void){
+	ios::sync_with_stdio(0);
+	fin.tie(0);
+	fout.tie(0);
+	solve();
+	return 0;
+}
