c4459ba49d9350d3f4a280b9906d551176a36b6c
Danilo <danilosindric@gmail.com>
2024-02-04 23:08:25 +0100

    solved adhoc

--- Codeforces/CopyCopyCopyCopyCopy/main.cpp
+++ Codeforces/CopyCopyCopyCopyCopy/main.cpp
@@ -6,43 +6,42 @@ using namespace std;
 #define pb push_back
 #define pii pair<int, int>
 #define vi vector<int>
-#define vii vector<pair<int,int>>
+#define vii vector<pair<int, int>>
 #define mp make_pair
-#define sz(a) a.size() 
+#define sz(a) a.size()
+#define MOD 1000000007
 #define forn(i, n) for (int i = 0; i < n; ++i)
 #define INF 1e18
 template <typename... T>
-void read(T &...args)
-{
+void read(T &...args){
 	((cin >> args), ...);
 }
 template <typename... T>
-void write(T... args)
-{
+void write(T... args){
 	((cout << args), ...);
 }
-const vector<pair<int, int>> nb = {{-1, -1}, {-1, 1}, {1, 1}, {1, -1}, {-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-const vector<pair<int, int>> nc = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+const vector<pair<int, int>> dirs1 = {{-1, -1}, {-1, 1}, {1, 1}, {1, -1}, {-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+const vector<pair<int, int>> dirs2 = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
 bool sortbysec(const pair<int, int> &a, const pair<int, int> &b) { return (a.second < b.second); }
-const int maxN = 2*1e5+1;
-
-
+const int maxN = 1e6;
 
 void solve(){
-	int n,x;
-	read(n);
+	int n;
 	set<int> s;
-	forn(i,n){
-		read(x);
-		s.insert(x);
+	int a;
+	read(n);
+	forn(i,n) {
+		read(a);
+		s.insert(a);
 	}
-	write(s.size(),"\n");
+	write(s.size(),'\n');
 }
 
 
 int main(void){
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+	cout.tie(0);
 	#ifndef ONLINE_JUDGE
 		freopen("input.txt", "r", stdin);
 		freopen("output.txt", "w", stdout);
@@ -52,4 +51,3 @@ int main(void){
 	while(t--)solve();
 	return 0;
 }
-
