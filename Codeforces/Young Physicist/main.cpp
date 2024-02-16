82e2ddd22a25701e955bca85120ec17df6d4da47
Danilo <danilosindric@gmail.com>
2024-02-16 20:49:13 +0100

    solved adhoc

---
+++ Codeforces/Young Physicist/main.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define pii pair<int, int>
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define mp make_pair
+#define sz(a) a.size()
+#define MOD 1000000007
+#define forn(i, n) for (int i = 0; i < n; ++i)
+#define INF 1e18
+template <typename... T>
+void read(T &...args){
+	((cin >> args), ...);
+}
+template <typename... T>
+void write(T... args){
+	((cout << args), ...);
+}
+const vector<pair<int, int>> dirs1 = {{-1, -1}, {-1, 1}, {1, 1}, {1, -1}, {-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+const vector<pair<int, int>> dirs2 = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+bool sortbysec(const pair<int, int> &a, const pair<int, int> &b) { return (a.second < b.second); }
+const int maxN = 1e6;
+
+
+void solve(){
+	int n;
+	read(n);
+	int x,y,z,sumx = 0, sumy = 0, sumz = 0;
+	for(int i=0;i<n;++i) {
+		read(x,y,z);
+		sumx += x;
+		sumy += y;
+		sumz += z;
+	}
+
+	if (sumx == 0 && sumy == 0 && sumz == 0)write("YES\n");
+	else write("NO\n");
+}
+
+
+int main(void){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+	solve();
+	return 0;
+}
