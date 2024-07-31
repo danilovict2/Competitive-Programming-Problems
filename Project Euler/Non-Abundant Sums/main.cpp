51c0620657be0969d8c80ad7746077337c4919ee
Danilo <danilosindric@gmail.com>
2024-07-31 21:58:31 +0200

    Solved using brute force

---
+++ Project Euler/Non-Abundant Sums/main.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define pii pair<int, int>
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define mp make_pair
+#define sz(a) a.size()
+#define MOD 1000000007
+#define forn(i, n) for (int i = 0; i < n; ++i)
+#define INF 1e18
+template <typename... T>
+void read(T &...args)
+{
+    ((cin >> args), ...);
+}
+template <typename... T>
+void write(T... args)
+{
+    ((cout << args), ...);
+}
+const vector<pair<int, int>> dirs1 = {{-1, -1}, {-1, 1}, {1, 1}, {1, -1}, {-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+const vector<pair<int, int>> dirs2 = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+bool sortbysec(const pair<int, int> &a, const pair<int, int> &b) { return (a.second < b.second); }
+const int maxN = 6000;
+
+int calc(int n) {
+    int sum = 0;
+    for(int i=1;i*2<=n;++i) {
+        if (n % i == 0)
+            sum += i;
+    }
+
+    return sum;
+}
+
+void solve(){
+    vector<int> abd;
+    abd.pb(12);
+    for(int i=13;i<=28123;++i) {
+        if (calc(i) > i)
+            abd.pb(i);
+    } 
+    
+    unordered_set<int> s;
+    for(auto x:abd) {
+        for(auto y:abd)
+            s.insert(x + y);
+    }
+
+    int ans = 0;
+    for(int i=1;i<=28123;++i)
+        if (!s.count(i))
+            ans+=i;
+    write(ans,'\n');
+    
+}
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    solve();
+    return 0;
+}
