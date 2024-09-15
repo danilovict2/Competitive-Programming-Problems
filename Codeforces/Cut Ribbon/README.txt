ab03436525c3c3828ceaec3f930aa82e27a34bfc
danilovict2 <danilosindric@gmail.com>
2024-09-15 17:39:13 +0200

    solved using dp

---
+++ Codeforces/Cut Ribbon/README.txt
@@ -0,0 +1,6 @@
+Polycarpus has a ribbon, its length is n. He wants to cut the ribbon in a way that fulfils the following two conditions:
+
+    After the cutting each ribbon piece should have length a, b or c.
+    After the cutting the number of ribbon pieces should be maximum. 
+
+Help Polycarpus and find the number of ribbon pieces after the required cutting.
---
+++ Codeforces/Cut Ribbon/main.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define pii pair<int, int>
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define mp make_pair
+#define sz(a) a.size()
+#define MOD 1000000007
+#define forn(i, n) for (int i = 0; i < n; ++i)
+#define INF 1e18
+template <typename... T>
+void read(T &...args)
+{
+	((cin >> args), ...);
+}
+template <typename... T>
+void write(T... args)
+{
+	((cout << args), ...);
+}
+const vector<pair<int, int>> dirs1 = {{-1, -1}, {-1, 1}, {1, 1}, {1, -1}, {-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+const vector<pair<int, int>> dirs2 = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+bool sortbysec(const pair<int, int> &a, const pair<int, int> &b) { return (a.second < b.second); }
+const int maxN = 2 * 1e5;
+
+int a,b,c;
+int dp[4001];
+
+int dfs(int n) {
+	if (n < 0)
+		return INT_MIN;
+	if (n == 0)
+		return 0;
+	if (dp[n] != -1)
+		return dp[n];
+		
+	return dp[n] = 1 + max({dfs(n-a), dfs(n-b), dfs(n-c)});
+}
+
+void solve() {
+	int n;
+	read(n,a,b,c);	
+	memset(dp, -1, sizeof(dp));
+	int ans = dfs(n);
+	write(ans, '\n');
+}
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+	solve();
+	return 0;
+}
