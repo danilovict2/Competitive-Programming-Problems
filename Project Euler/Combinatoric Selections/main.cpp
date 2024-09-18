857ec466ed1e64763efc4e7d8a92ccaed333023c
danilovict2 <danilosindric@gmail.com>
2024-09-18 22:09:51 +0200

    Solved using brute force

---
+++ Project Euler/Combinatoric Selections/main.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define pii pair<int, int>
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define mp make_pair
+#define sz(a) a.size()
+#define MOD 1000000007
+#define forn(i, n) for (int i = 0; i < n; ++i)
+#define INF 1e18
+template <typename... T>
+void read(T &...args)
+{
+    ((cin >> args), ...);
+}
+template <typename... T>
+void write(T... args)
+{
+    ((cout << args), ...);
+}
+const vector<pair<int, int>> dirs1 = {{-1, -1}, {-1, 1}, {1, 1}, {1, -1}, {-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+const vector<pair<int, int>> dirs2 = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+bool sortbysec(const pair<int, int> &a, const pair<int, int> &b) { return (a.second < b.second); }
+const int maxN = 1e6;
+
+void solve() {
+    int ans = 0;
+    for(int n=1;n<=100;++n) {
+        for(int r=1;r<=n;++r) {
+            long double prod = 1;
+            for(long double i=n;i>max(n-r, r);--i)
+                prod *= i;
+            for (long double i=2;i<=min(n-r, r);++i)
+                prod /= i;
+            write(n,' ', r,' ', prod, '\n');
+            if (prod > 1000000)ans++;
+        }
+    }
+
+    write(ans, '\n');
+}
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    solve();
+    return 0;
+}
