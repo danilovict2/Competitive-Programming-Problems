389e10cdb5c44c98d3059a6e431087b89393f6bb
Danilo Sindric <danilosindric@gmail.com>
2025-10-25 21:16:14 -0400

    Update: improve solution to use two pointers

--- leetcode/Trapping Rain Water/main.py
+++ leetcode/Trapping Rain Water/main.py
@@ -1,17 +1,17 @@
 class Solution:
     def trap(self, height: list[int]) -> int:
-        n = len(height)
-        prefix = [0] * n
-        suffix = [0] * n
-        for i in range(1, n):
-            prefix[i] = max(prefix[i - 1], height[i - 1])
-
-        for i in range(n - 2, -1, -1):
-            suffix[i] = max(suffix[i + 1], height[i + 1])
-
+        l, r = 0, len(height) - 1
+        max_l, max_r = height[l], height[r]
         s = 0
-        for i in range(n):
-            s += max(0, min(prefix[i], suffix[i]) - height[i]) 
+        while l < r:
+            if max_l <= max_r:
+                l += 1
+                s += max(0, max_l - height[l])
+                max_l = max(max_l, height[l])
+            else:
+                r -= 1
+                s += max(0, max_r - height[r])
+                max_r = max(max_r, height[r])
 
         return s
-
+            
