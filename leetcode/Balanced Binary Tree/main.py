bb704b59654d8b6082f53b51051d7c74ea12aa1d
Danilo Sindric <danilosindric@gmail.com>
2025-11-12 17:59:57 -0500

    Add: solved using dfs

---
+++ leetcode/Balanced Binary Tree/main.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        return self.dfs(root)[0]
+    
+    def dfs(self, root):
+        if not root:
+            return [True, 0]
+        
+        left, right = self.dfs(root.left), self.dfs(root.right)
+        ok = left[0] and right[0] and abs(left[1] - right[1]) <= 1
+        return [ok, 1 + max(left[1], right[1])]
