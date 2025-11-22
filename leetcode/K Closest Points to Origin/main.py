eb28709101ae78f1e6352731ec18a7e7c014cc48
Danilo Sindric <danilosindric@gmail.com>
2025-11-21 20:36:09 -0500

    Add: solved using max heap

---
+++ leetcode/K Closest Points to Origin/main.py
@@ -0,0 +1,11 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        h = []
+        for point in points:
+            dist = (point[0] ** 2 + point[1] ** 2)
+            heapq.heappush(h, [-dist, point])
+            if len(h) > k:
+                heapq.heappop(h)
+        
+        return [x[1] for x in h]
+        
