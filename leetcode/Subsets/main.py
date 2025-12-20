05df59a496c84e08ecf87d74f4855d66c594ee8c
Danilo Sindric <danilosindric@gmail.com>
2025-12-20 17:42:19 +0100

    Add: solved using dfs

---
+++ leetcode/Subsets/main.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        subsets = []
+        def dfs(i, subset = []):
+            if i >= len(nums):
+                subsets.append(subset.copy())
+                return
+            
+            subset.append(nums[i])
+            dfs(i + 1, subset)
+            subset.pop()
+            dfs(i + 1, subset)
+        
+        dfs(0)
+        return subsets
+        
