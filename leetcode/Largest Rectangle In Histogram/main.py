e90721fe7a6223211e3f67581d5082c60ee2aa7a
Danilo Šindrić <93711398+danilovict2@users.noreply.github.com>
2025-12-27 14:34:46 +0100

    Refactor largestRectangleArea method
    Refactor largestRectangleArea to simplify calculations and improve readability.

--- leetcode/Largest Rectangle In Histogram/main.py
+++ leetcode/Largest Rectangle In Histogram/main.py
@@ -1,31 +1,16 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        n = len(heights)
-        left, right = [-1] * n, [n] * n
-        stack = []
-        for i in range(n):
-            while len(stack) > 0 and heights[i] <= heights[stack[-1]]:
-                stack.pop()
-
-            if len(stack) > 0:
-                left[i] = stack[-1]
-
-            stack.append(i)
-
-        stack = []
-        for i in range(n-1, -1, -1):
-            while len(stack) > 0 and heights[i] <= heights[stack[-1]]:
-                stack.pop()
-            
-            if len(stack) > 0:
-                right[i] = stack[-1]
-            
-            stack.append(i)
-
         ans = 0
-        for i in range(n):
-            left[i] += 1
-            right[i] -= 1
-            ans = max(ans, heights[i] * (right[i] - left[i] + 1))
+        stack = []
+
+        for i, h in enumerate(heights):
+            start = i
+            while stack and stack[-1][1] > h:
+                index, height = stack.pop()
+                ans = max(ans, height * (i - index))
+                start = index
+            stack.append((start, h))
         
+        for i, h in stack:
+            ans = max(ans, h * (len(heights) - i))
         return ans
