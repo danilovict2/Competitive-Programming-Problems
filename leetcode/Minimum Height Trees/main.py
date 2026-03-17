49850d27144e29cf81818eff9b0a06e52c109112
Danilo Sindric <danilosindric@gmail.com>
2026-03-16 22:24:51 -0400

    Add: solved using topological sort

---
+++ leetcode/Minimum Height Trees/main.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        if n == 1:
+            return [0]
+        
+        adj = defaultdict(list)
+        for a, b in edges:
+            adj[a].append(b)
+            adj[b].append(a)
+        
+        leaves = deque()
+        edge_count = {}
+
+        for src, neighbours in adj.items():
+            edge_count[src] = len(neighbours)
+            if len(neighbours) == 1:
+                leaves.append(src)
+        
+        while leaves:
+            if n <= 2:
+                return list(leaves)
+
+            for _ in range(len(leaves)):
+                node = leaves.popleft()
+                n -= 1
+                for nei in adj[node]:
+                    edge_count[nei] -= 1
+                    if edge_count[nei] == 1:
+                        leaves.append(nei)
+
