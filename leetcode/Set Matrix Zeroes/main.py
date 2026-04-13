be5da2585d08d2834bafff7a98b4cdcd9e1ff95d
Danilo Šindrić <93711398+danilovict2@users.noreply.github.com>
2026-04-12 23:09:00 -0400

    Optimizations

--- leetcode/Set Matrix Zeroes/main.py
+++ leetcode/Set Matrix Zeroes/main.py
@@ -1,17 +1,15 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         m, n = len(matrix), len(matrix[0])
-        firstRow = matrix[0]
-        for j in range(n):
-            if matrix[0][j] == 0:
-                firstRow = [0] * n
-                break
-
-        for i in range(1, m):
+        rowZero = False
+        for i in range(m):
             for j in range(n):
                 if matrix[i][j] == 0:
-                    matrix[i][0] = 0
                     matrix[0][j] = 0
+                    if i > 0:
+                        matrix[i][0] = 0
+                    else:
+                        rowZero = True
         
         
         for i in range(1, m):
@@ -19,5 +17,6 @@ class Solution:
                 if matrix[i][0] == 0 or matrix[0][j] == 0:
                     matrix[i][j] = 0
 
-        matrix[0] = firstRow
+        if rowZero:
+            matrix[0] = [0] * n
         
